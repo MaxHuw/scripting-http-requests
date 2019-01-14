aa7fb47a505d05be6960caa2aaa78dbd33f4ed5f
Max Graham <m.huw.graham@gmail.com>
2019-01-14 17:28:00 +0000

    Step 4 - Using callback functions

---
+++ step-four.js
@@ -0,0 +1,34 @@
+var https = require('https');
+
+//var options = process.argv.slice(2);
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+function getHTML(options, callback) {
+
+var data = '';
+
+  https.get(options, function(response){
+
+    response.setEncoding('utf8');
+
+    response.on('data', function(chunk){
+      data += chunk.toString();
+      //console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
+    });
+
+    response.on('end', function(){
+      callback(data);
+      console.log('Response stream complete.');
+    });
+  });
+};
+
+function printHTML(html){
+  console.log(html);
+}
+
+getHTML(requestOptions, printHTML);
