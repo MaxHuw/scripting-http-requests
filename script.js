c9fa83ba0a3ea9eb7bd911b49fe2c10eef482181
Max Graham <m.huw.graham@gmail.com>
2019-01-14 16:24:03 +0000

    First step done, can print data in chunks.

---
+++ script.js
@@ -0,0 +1,29 @@
+
+
+function getAndPrintHTMLChunks () {
+
+  var https = require('https');
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  https.get(requestOptions, function(response){
+
+    response.setEncoding('utf8');
+
+    response.on('data', function(chunk){
+      console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
+      console.log(chunk.toString());
+    });
+
+    response.on('end', function(){
+      console.log('Response stream complete.');
+    });
+
+  });
+
+}
+
+getAndPrintHTMLChunks();
